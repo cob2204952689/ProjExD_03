a4159070b18847b45037ca770086450c714a0865
kizami <c0b2204952@edu.teu.ac.jp>
2023-10-17 17:29:37 +0900

    演習3

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -10,6 +10,7 @@ HEIGHT = 900  # ゲームウィンドウの高さ
 NUM_OF_BOMBS = 5
 
 
+
 def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
     """
     オブジェクトが画面内or画面外を判定し，真理値タプルを返す関数
@@ -138,6 +139,18 @@ class Bomb:
             self.vy *= -1
         self.rct.move_ip(self.vx, self.vy)
         screen.blit(self.img, self.rct)
+
+
+class Score:
+    def __init__(self):
+        self.font = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 30)
+        self.scr =0
+        self.img = self.font.render(f"スコア:{self.scr}", 0, (0,0,255))
+        self.rct = self.img.get_rect()
+        self.rct.center = (100,850)
+    def update(self, screen: pg.Surface):
+        self.img = self.font.render(f"スコア:{self.scr}", 0, (0,0,255))
+        screen.blit(self.img, self.rct)
         
 
 
@@ -148,6 +161,9 @@ def main():
     bird = Bird(3, (900, 400))
     bombs = [Bomb() for _ in range(NUM_OF_BOMBS)]
     beam = None
+    score = Score()
+    
+    
 
     clock = pg.time.Clock()
     tmr = 0
@@ -165,6 +181,7 @@ def main():
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
+                score.update(screen)
                 pg.display.update()
                 time.sleep(1)
                 return
@@ -174,7 +191,9 @@ def main():
                     beam = None
                     bombs[i] = None
                     bird.change_img(6,screen)
+                    score.scr += 1
                     pg.display.update()
+                
         bombs = [bomb for bomb in bombs if bomb is not None]
 
         key_lst = pg.key.get_pressed()
@@ -184,6 +203,7 @@ def main():
             bomb.update(screen)
         if beam is not None:
             beam.update(screen)
+        score.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
